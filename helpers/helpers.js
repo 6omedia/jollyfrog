bab18d12790031ca3dc33bfaa28495f59d986171
Mike <mike@6omedia.co.uk>
2017-10-24 14:14:12 +0100

    tracking code changes

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,10 +2,11 @@ var helpers = {};
 
 function getTrackingCode(domain, apikey){
 
-	var code = '(function(d,j,ak,c){';
+	var code = '(function(d,j,ak){';
     code += 'd.addEventListener("DOMContentLoaded", function(){';
+    code += 'var c = new ClientJS();';
     code += 'j.doIt(ak,c);});';
-    code += '}(document, new JFrog("http://' + domain + '"),"' + apikey + '", new ClientJS()));';
+    code += '}(document, new JFrog("http://' + domain + '"),"' + apikey + '"));';
 
 	return code;
 
